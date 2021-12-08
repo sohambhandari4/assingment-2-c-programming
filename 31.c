834b5bddc19a444a091630ba4144fe1617db921d
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 12:47:46 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -1,27 +1,19 @@
-/**
- * C program to find sum of its digits of a number
- */
+//Program to calculate the x to the power y without using standard function.
 
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<conio.h>
+void main()
 {
-    int num, sum=0;
-
-    /* Input a number from user */
-    printf("Enter any number to find sum of its digit: ");
-    scanf("%d", &num);
-
-    /* Repeat till num becomes 0 */
-    while(num!=0)
-    {
-        /* Find last digit of num and add to sum */
-        sum += num % 10;
-
-        /* Remove last digit from num */
-        num = num / 10;
-    }
-
-    printf("Sum of digits = %d", sum);
-
-    r
+ int x,y,i,r=1,t;
+ printf("Enter a number:");
+ scanf("%d",&x);
+ printf("Enter the power:");
+ scanf("%d",&y);
+ for(i=1;i<=y;i++)
+ {
+  t=x;
+  r=r*t;
+ }
+ printf("Result:%d",r);
+ getch();
+}
