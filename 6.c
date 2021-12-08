01a875f63b7dd2f00f1d111a451a928839f07eba
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 11:41:19 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,47 +1,24 @@
 /**
- * C program to find maximum between three numbers using nested if
+ * C program to find minimum between three numbers 
  */
 
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int num1, num2, num3, max;
-
-    /* Input three numbers from user */
-    printf("Enter three numbers: ");
-    scanf("%d%d%d", &num1, &num2, &num3);
-    
-
-    if(num1 > num2)
-    {
-        if(num1 > num3)
-        {
-            /* If num1 > num2 and num1 > num3 */
-            max = num1;
-        }
-        else
-        {
-            /* If num1 > num2 but num1 > num3 is not true */
-            max = num3;
-        }
-    }
-    else
-    {
-        if(num2 > num3)
-        {
-            /* If num1 is not > num2 and num2 > num3 */
-            max = num2;
-        }
-        else
-        {
-            /* If num1 is not > num2 and num2 > num3 */
-            max = num3;
-        }
-    }
-    
-    /* Print maximum value */
-    printf("Maximum among all three numbers = %d", max);
-
-    return 0;
+	int num1,num2,num3;
+	printf("Enter three numbers:");
+	scanf("%d %d %d"&num1,&num2,&num3);
+	if(num1 < num2 && num1 < num3)
+	{
+		printf("%d is smallest",num1);
+	}
+	else if(num2 < num3)
+	{
+		printf("%d is smallest",num2);
+	}
+	else
+	{
+		printf("%d is smallest",num3);
+	}	
+	return 0;
 }
