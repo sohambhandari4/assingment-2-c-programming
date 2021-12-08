3c418216664caa8d151f9b1949d4e145b9ab8143
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 12:17:11 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -1,19 +1,16 @@
-#include<stdio.h>  
- int main()    
-{    
-int n,r,sum=0,temp;    
-printf("enter the number=");    
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
-{    
-r=n%10;    
-sum=sum+(r*r*r);    
-n=n/10;    
-}    
-if(temp==sum)    
-printf("armstrong  number ");    
-else    
-printf("not armstrong number");    
-return 0;  
-}   
+//even Or odd
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // true if num is perfectly divisible by 2
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    
+    return 0;
+}
