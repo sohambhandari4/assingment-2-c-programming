fc45b3ca49fa77d7cc74902ab9d54bc092abaf89
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 12:23:08 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -14,14 +14,6 @@ int main()
     printf("Enter any year: ");
     scanf("%d", &year);
 
-    /*
-     * If year%4==0 and year%100==0 then
-     *     print leap year
-     * else if year%400==0 then
-     *     print leap year
-     * else
-     *     print common year 
-     */
     (year%4==0 && year%100!=0) ? printf("LEAP YEAR") :
         (year%400 ==0 ) ? printf("LEAP YEAR") : printf("COMMON YEAR");
 
