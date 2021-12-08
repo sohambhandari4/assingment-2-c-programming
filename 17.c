f86eb62888dc194889fb0f6064d5789fe7ce5c06
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 12:19:07 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,26 +1,21 @@
-// leap year or not
+//  to check whether the enter number is Armstrong or not.
 
-#include <stdio.h>
-int main() {
-   int year;
-   printf("Enter a year: ");
-   scanf("%d", &year);
-
-   // leap year if perfectly divisible by 400
-   if (year % 400 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   // not a leap year if divisible by 100
-   // but not divisible by 400
-   else if (year % 100 == 0) {
-      printf("%d is not a leap year.", year);
-   }
-   // leap year if not divisible by 100
-   // but divisible by 4
-   else if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   // all other years are not leap years
-   else {
-      printf("%d is not a leap year.", year);
-   }
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}   
