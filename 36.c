f72ad52913f7bdb433080d43a87914e5a5d85ef5
sohambhandari4 <87934665+sohambhandari4@users.noreply.github.com>
2021-12-08 12:57:50 +0530

    Update 36.c

--- 36.c
+++ 36.c
@@ -1,26 +1,18 @@
+//Program to find power of a number (xn)
+
 #include <stdio.h>
 int main() {
-  int n, i, flag = 0;
-  printf("Enter a positive integer: ");
-  scanf("%d", &n);
+    int base, exp;
+    long double result = 1.0;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
 
-  for (i = 2; i <= n / 2; ++i) {
-    // condition for non-prime
-    if (n % i == 0) {
-      flag = 1;
-      break;
+    while (exp != 0) {
+        result *= base;
+        --exp;
     }
-  }
-
-  if (n == 1) {
-    printf("1 is neither prime nor composite.");
-  } 
-  else {
-    if (flag == 0)
-      printf("%d is a prime number.", n);
-    else
-      printf("%d is not a prime number.", n);
-  }
-
-  return 0;
+    printf("Answer = %.0Lf", result);
+    return 0;
 }
